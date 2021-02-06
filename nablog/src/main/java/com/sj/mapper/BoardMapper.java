a11cc787bb5e739bb69e13970354807cc5a0ae69
kyzuna <kyzuna@naver.com>
2021-02-06 23:26:12 +0900

    spring 설정

---
+++ nablog/src/main/java/com/sj/mapper/BoardMapper.java
@@ -0,0 +1,11 @@
+package com.sj.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface BoardMapper {
+	
+	@Select("SELECT sysdate FROM dual")
+	public String getTime();
+	
+	public String getTime2();
+}
